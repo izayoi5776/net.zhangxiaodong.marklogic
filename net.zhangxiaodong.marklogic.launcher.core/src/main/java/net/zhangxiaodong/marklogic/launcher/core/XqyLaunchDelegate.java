e77a656540370b41c187813bc79483b1fe127d98
zxd <izayoi5776@gmail.com>
2017-02-26 13:39:14 +0900

    run demo
    can demo run, add class path need to run

--- net.zhangxiaodong.marklogic.launcher.core/src/main/java/net/zhangxiaodong/marklogic/launcher/core/XqyLaunchDelegate.java
+++ net.zhangxiaodong.marklogic.launcher.core/src/main/java/net/zhangxiaodong/marklogic/launcher/core/XqyLaunchDelegate.java
@@ -1,11 +1,22 @@
 package net.zhangxiaodong.marklogic.launcher.core;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
 import org.eclipse.jdt.launching.JavaLaunchDelegate;
+
+import com.marklogic.xcc.ContentSource;
+import com.marklogic.xcc.ContentSourceFactory;
+import com.marklogic.xcc.Request;
+import com.marklogic.xcc.ResultSequence;
+import com.marklogic.xcc.Session;
+import com.marklogic.xcc.exceptions.RequestException;
+import com.marklogic.xcc.exceptions.XccConfigException;
 
 public class XqyLaunchDelegate extends JavaLaunchDelegate implements ILaunchConfigurationDelegate {
 
@@ -16,6 +27,18 @@ public class XqyLaunchDelegate extends JavaLaunchDelegate implements ILaunchConf
 	    ILaunch launch, 
 	    IProgressMonitor monitor) throws CoreException {
 			System.out.print("test");
+			try {
+				run();
+			} catch (XccConfigException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RequestException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 	@Override
@@ -23,4 +46,17 @@ public class XqyLaunchDelegate extends JavaLaunchDelegate implements ILaunchConf
 			String mode, IProgressMonitor monitor) throws CoreException {
 				return true;
 	}
+	
+	private void run() throws URISyntaxException, XccConfigException, RequestException{
+    	URI uri = new URI("xcc://admin:admin@localhost:8021/");
+    	ContentSource contentSource = 
+    	          ContentSourceFactory.newContentSource (uri);
+
+    	Session session = contentSource.newSession();
+    	Request request = session.newAdhocQuery ("/nodes/node[1 to 10]");
+    	ResultSequence rs = session.submitRequest (request);
+    	System.out.println (rs.asString());
+    	session.close();
+	}
+	
 }
