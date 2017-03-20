1747f00e156a17c78ca808420494acb528347555
zxd <izayoi5776@gmail.com>
2017-03-20 21:18:44 +0900

    add default value

---
+++ net.zhangxiaodong.marklogic.launcher.ui/src/main/java/net/zhangxiaodong/marklogic/launcher/ui/IXqyLaunchConfigurationConstants.java
@@ -0,0 +1,9 @@
+package net.zhangxiaodong.marklogic.launcher.ui;
+
+public interface IXqyLaunchConfigurationConstants {
+
+	public static final String ATTR_SERVER_NAME = "SERVER_NAME";	 //$NON-NLS-1$
+	public static final String ATTR_SERVER_PORT = "SERVER_PORT";	 //$NON-NLS-1$
+	public static final String ATTR_USER_NAME =  "USER_NAME";	 //$NON-NLS-1$
+	public static final String ATTR_USER_PASS = "USER_PASS";	 //$NON-NLS-1$
+}
--- net.zhangxiaodong.marklogic.launcher.ui/src/main/java/net/zhangxiaodong/marklogic/launcher/ui/XqueryMainTab.java
+++ net.zhangxiaodong.marklogic.launcher.ui/src/main/java/net/zhangxiaodong/marklogic/launcher/ui/XqueryMainTab.java
@@ -1,12 +1,17 @@
 package net.zhangxiaodong.marklogic.launcher.ui;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.internal.ui.SWTFactory;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.jdt.internal.debug.ui.IJavaDebugHelpContextIds;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.internal.debug.ui.actions.ControlAccessibleListener;
 import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jface.internal.databinding.swt.WidgetListener;
+import org.eclipse.swt.events.ModifyListener;
 //import org.eclipse.jdt.internal.debug.ui.launcher.AbstractJavaMainTab.WidgetListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -14,21 +19,23 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 //import org.eclipse.jdt.internal.debug.ui.launcher.AbstractJavaMainTab.WidgetListener;
 
 public class XqueryMainTab extends AbstractLaunchConfigurationTab {
 
 	//Project UI widgets
-	protected Text fProjText;
+	protected Text fProjText, fText1, fText2, fText3, fText4, fText5;
 	private Button fProjButton;
-	//private WidgetListener fListener = new WidgetListener();
+	private WidgetListener fListener = new WidgetListener(null, null, null, null);
 		
 	@Override
 	public void createControl(Composite parent) {
 		Composite projComp = SWTFactory.createComposite(parent, parent.getFont(), 1, 1, GridData.FILL_BOTH); 
 		((GridLayout)projComp.getLayout()).verticalSpacing = 0;
-		createProjectEditor(projComp);
+		//createProjectEditor(projComp);
+		createXqueryEnv(projComp);
 		createVerticalSpacer(projComp, 1);
 		//createMainTypeEditor(projComp, LauncherMessages.appletlauncher_maintab_mainclasslabel_name);
 		createVerticalSpacer(projComp, 1);
@@ -41,30 +48,61 @@ public class XqueryMainTab extends AbstractLaunchConfigurationTab {
 	 * 
 	 * @param parent the parent composite
 	 */
-	protected void createProjectEditor(Composite parent) {
-		Group group = SWTFactory.createGroup(parent, LauncherMessages.AbstractJavaMainTab_0, 2, 1, GridData.FILL_HORIZONTAL);
-		fProjText = SWTFactory.createSingleText(group, 1);
-		//fProjText.addModifyListener(fListener);
-		ControlAccessibleListener.addListener(fProjText, group.getText());
-		fProjButton = createPushButton(group, LauncherMessages.AbstractJavaMainTab_1, null); 
-		//fProjButton.addSelectionListener(fListener);
-	}	
+	protected void createXqueryEnv(Composite parent) {
+		Group group = SWTFactory.createGroup(parent, "&File name", 2, 1, GridData.FILL_HORIZONTAL);
+
+		SWTFactory.createLabel(group, "Run file:", 1);
+		fText1 = SWTFactory.createSingleText(group, 1);
+
+		SWTFactory.createLabel(group, "Server:", 1);
+		fText2 = SWTFactory.createSingleText(group, 1);
+		
+		SWTFactory.createLabel(group, "Port:", 1);
+		fText3 = SWTFactory.createSingleText(group, 1);
+		
+		SWTFactory.createLabel(group, "Username:", 1);
+		fText4 = SWTFactory.createSingleText(group, 1);
+
+		SWTFactory.createLabel(group, "Password:", 1);
+		fText5 = SWTFactory.createSingleText(group, 1);
+		//fProjText.addModifyListener((ModifyListener) fListener);
+		//ControlAccessibleListener.addListener(fProjText, group.getText());
+	}
+	
 	@Override
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void initializeFrom(ILaunchConfiguration configuration) {
-		// TODO Auto-generated method stub
-
+		try {
+			fText2.setText((configuration.getAttribute(IXqyLaunchConfigurationConstants.ATTR_SERVER_NAME, "localhost"))); 
+		} catch(CoreException ce) {
+			fText2.setText("localhost"); 
+		}
+		try {
+			fText3.setText((configuration.getAttribute(IXqyLaunchConfigurationConstants.ATTR_SERVER_PORT, "8021"))); 
+		} catch(CoreException ce) {
+			fText3.setText("8021"); 
+		}
+		try {
+			fText4.setText((configuration.getAttribute(IXqyLaunchConfigurationConstants.ATTR_USER_NAME, "admin"))); 
+		} catch(CoreException ce) {
+			fText4.setText("admin"); 
+		}
+		try {
+			fText5.setText((configuration.getAttribute(IXqyLaunchConfigurationConstants.ATTR_USER_PASS, "admin"))); 
+		} catch(CoreException ce) {
+			fText5.setText("admin"); 
+		}
 	}
 
 	@Override
 	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-		// TODO Auto-generated method stub
-
+		configuration.setAttribute(IXqyLaunchConfigurationConstants.ATTR_SERVER_NAME, fText2.getText());
+		configuration.setAttribute(IXqyLaunchConfigurationConstants.ATTR_SERVER_PORT, fText3.getText());
+		configuration.setAttribute(IXqyLaunchConfigurationConstants.ATTR_USER_NAME, fText4.getText());
+		configuration.setAttribute(IXqyLaunchConfigurationConstants.ATTR_USER_PASS, fText5.getText());
 	}
 
 	@Override
